851c299c580841539e7da01646ca1df740ea4236
Davi Fonseca <davi.brandao2013@gmail.com>
2020-03-22 19:15:34 -0300

    foi ?

--- src/pais/PaisTeste.java
+++ src/pais/PaisTeste.java
@@ -1,11 +1,35 @@
 package pais;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import testesunitarios.Cliente;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PaisTeste {
 	
 	
+@Before
+public void inserindo() throws Exception  {
+	System.out.println("Inserindo");
+	pais = new Pais (id, "Japão", 126440000, 377975);
+	copia = new Pais (id, "Japão", 126440000, 377975);
+	System.out.println(pais);
+	System.out.println(copia);
+	System.out.println(id);
+}
+
+@Test
+public void test00Carregar() {
+	System.out.println("Carregar");
+	Pais esperado = new Pais(1, "Japão", 126440000, 377975);
+	Pais retorno = new Pais(1, null, null, null);
+	retorno.carregarPaises();
+	assertEquals("Testa Inclusão", retorno, esperado);
+}
 	
 }
